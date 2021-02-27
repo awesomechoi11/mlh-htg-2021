7f893db06f605d6119a952f88960119bf48f6f2b
brandon <brandonchoi2016@gmail.com>
2021-02-26 19:57:12 -0800

    some assets and routing

--- mlh-htg-website/src/App.js
+++ mlh-htg-website/src/App.js
@@ -1,12 +1,101 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+} from "react-router-dom";
+
 import './App.scss'
+
+import Dashboard from './js/dashboard/dashboard'
+import Search from './js/search/search'
+import Home from './js/home/home'
+import About from './js/about/about'
+import Login from './js/login/login'
 
 function App() {
 
   return (
-    <div className="App">
+    <Router>
+      <Switch>
 
-    </div>
+        {routes.map((item, index) => (
+          <Route key={index} path={item.to} >
+            <>
+              {item.component}
+              {item.navbar && <NavBar />}
+            </>
+          </Route>
+        ))}
+
+      </Switch>
+    </Router>
   );
 }
+
+export function CustomLink(props) {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push(props.to);
+  }
+  return (
+    <div {...props}
+      onClick={handleClick}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+function NavBar() {
+
+  return (
+
+    <div className="nav-bar">
+      {routes.map((item, index) => (
+        item.navbar &&
+        <CustomLink key={index} to={item.to} className="nav-item">
+          {item.children && item.children}
+        </CustomLink>
+
+      ))}
+
+    </div>
+  )
+}
+
+const routes = [
+  {
+    to: '/login',
+    children: <div className='nav-login'>LOGIN</div>,
+    component: <Login />,
+    navbar: true
+  },
+  {
+    to: '/dashboard',
+    children: <div className='nav-account'>ACCOUNT</div>,
+    component: <Dashboard />,
+    navbar: false
+  }, {
+    to: '/search',
+    children: <div className='nav-search'>SEARCH</div>,
+    component: <Search />,
+    navbar: true
+  }, {
+    to: '/about',
+    children: <div className='nav-about'>ABOUT US</div>,
+    component: <About />,
+    navbar: true
+  }, {
+    to: '/',
+    children: (
+      <div className='nav-about'>HOME</div>
+    ),
+    component: <Home />,
+    navbar: true
+  }
+]
 
 export default App;
---
+++ mlh-htg-website/src/js/login/login.js
@@ -0,0 +1,13 @@
+import '../../sass/login.scss';
+
+
+export default function Login() {
+
+
+    return (
+        <div className='search-page' >
+
+        </div>
+    )
+
+}
