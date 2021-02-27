30c227461ac84b3f5952400356b4cb3c347d93a9
brandon <brandonchoi2016@gmail.com>
2021-02-27 03:52:56 -0800

    middle home dashboard done

--- mlh-htg-website/src/js/dashboard/Connections.js
+++ mlh-htg-website/src/js/dashboard/Connections.js
@@ -22,8 +22,8 @@ export default function Connections() {
                 .replace(/(:\d{2})$/, "")
             var date = faker.date.between('2021-02-29', '2021-03-20').toLocaleString()
             date = date.split(", ")
-            date = date[0]
-            console.log(faker.date.between('2021-02-29', '2021-03-20'))
+            date = faker.date.past().toDateString()
+            console.log(faker)
             setNonProfits(snapshot.docs.map(doc => ({ time, date, email, phoneNumber, ...doc.data() })))
         })
         // getPhotoUrl().then(url => {
@@ -55,7 +55,7 @@ function ConnectPanel(props) {
         'accepting',
         'closed'
     ]
-
+    const currstatus = status[Math.floor(Math.random() * Math.floor(3))]
     function schedule() {
         props.schedule({ name: props.name, time: props.time, date: props.date, address: props.address })
         setScheduled(true)
@@ -68,13 +68,21 @@ function ConnectPanel(props) {
             <div className="nonProfitInfo">
                 <p className='title'>{props.name}</p>
                 <p className='number'>{props.phoneNumber}</p>
-                <p className='time'>PICKUP AVAILABLE: {"\xa0" + props.time + "\xa0\xa0" + props.date}</p>
+                <p className='time'>PICKUP AVAILABLE: {"\xa0\xa0" + props.date}</p>
             </div>
 
             <div className="nonProfitStatus">
-                {!scheduled ? <p>STATUS: ACCEPTING</p> : <p>STATUS: SCHEDULED</p>}
-                <p>GOTO PROFILE</p>
-                {!scheduled && <button onClick={schedule}>Schedule</button>}
+                <div className={'status-pill ' + currstatus}>
+                    {currstatus}
+                </div>
+                <div className='view-btn'>
+                    view organization profile {' '}
+                    <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1.39526 8.5H15.3953" stroke="#EAE9DF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M8.39526 1.5L15.3953 8.5L8.39526 15.5" stroke="#EAE9DF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+
+                </div>
             </div>
         </div>
     )
